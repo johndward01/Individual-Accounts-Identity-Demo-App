f97b6a9608de6239409005599ae41ec5b893c496
jdw <johndward01@gmail.com>
2021-10-31 00:05:31 -0500

    Add project files.

---
+++ Individual Accounts Identity Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Individual_Accounts_Identity_Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
